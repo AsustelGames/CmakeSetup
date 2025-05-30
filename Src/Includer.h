155a7000e340701454adb2e8ad7e64968e63ecd2
AsustelGames <alratna2027@gmail.com>
2025-05-30 09:12:21 +0200

    a

---
+++ Src/Includer.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <iostream>
+
+#include "SDL3/SDL.h"
+#include "SDL3_mixer/SDL_mixer.h"
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h""
+#include "enet/enet.h"
+#include "FastNoiseLite.h"
+#include "stb_image.h"
+#include "box2d/box2d.h"
+
+
---
+++ Src/Main.cpp
@@ -0,0 +1,56 @@
+﻿#include "Includer.h"
+
+int main()
+{
+    SDL_Window* Window = SDL_CreateWindow("SDLProjectTemplate - Window", 640, 480, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* Renderer = SDL_CreateRenderer(Window, NULL);
+    bool ShouldWindowClose = false;
+
+    ImGui::CreateContext();
+    ImGuiIO IO = ImGui::GetIO(); (void)IO;
+
+    ImGui_ImplSDL3_InitForSDLRenderer(Window, Renderer);
+    ImGui_ImplSDLRenderer3_Init(Renderer);
+    
+    while (!ShouldWindowClose)
+    {
+        SDL_Event Event;
+        
+        while (SDL_PollEvent(&Event))
+        {
+            ImGui_ImplSDL3_ProcessEvent(&Event);
+            if (Event.type == SDL_EVENT_QUIT)
+            {
+                ShouldWindowClose = true;
+            }
+        }
+
+        ImGui_ImplSDL3_NewFrame();
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("test", NULL);
+        if (ImGui::Button("click", { 100, 100 }))
+        {
+            std::cout << "a" << '\n';
+        }
+        ImGui::End();
+
+        ImGui::Render();
+        SDL_SetRenderDrawColor(Renderer, 100, 100, 100, 255);
+        SDL_RenderClear(Renderer);
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), Renderer);
+        SDL_RenderPresent(Renderer);
+    }
+    std::cout << "hello" << '\n';
+
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(Renderer);
+    SDL_DestroyWindow(Window);
+    SDL_Quit();
+
+    return 0;
+}
